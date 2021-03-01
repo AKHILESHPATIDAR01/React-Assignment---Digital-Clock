fb35266a1b4b6434dda848abbd6c83fa6f51ba54
AKHILESHPATIDAR01 <71465599+AKHILESHPATIDAR01@users.noreply.github.com>
2021-03-01 16:42:49 +0530

    Update App.js
    ccccc

--- src/components/App.js
+++ src/components/App.js
@@ -86,16 +86,23 @@ class App extends React.Component {
 //             </div>
 //         );
             
-                let hours = this.state.date.getHours() < 12 ? this.state.date.getHours() : this.state.date.getHours() - 12;
-                let minutes = this.state.date.getMinutes();
-                let seconds = this.state.date.getSeconds();
-                let ampm = this.state.date.getHours() >= 12 ? 'PM' : 'AM';
-                return (
-                    <div className="Clock">
+//                 let hours = this.state.date.getHours() < 12 ? this.state.date.getHours() : this.state.date.getHours() - 12;
+//                 let minutes = this.state.date.getMinutes();
+//                 let seconds = this.state.date.getSeconds();
+//                 let ampm = this.state.date.getHours() >= 12 ? 'PM' : 'AM';
+//                 return (
+//                     <div className="Clock">
+//                         <h3 id="time">
+//                             {hours == 0 ? 12 : hours > 12 ? hours - 12 : hours}:
+//                             {minutes > 9 ? minutes : `0${minutes}`}:
+//                             {seconds > 9 ? seconds : `0${seconds}`} {ampm}
+//                         </h3>
+//                     </div>
+//                 );
+           return (
+                   <div className="Clock">
                         <h3 id="time">
-                            {hours == 0 ? 12 : hours > 12 ? hours - 12 : hours}:
-                            {minutes > 9 ? minutes : `0${minutes}`}:
-                            {seconds > 9 ? seconds : `0${seconds}`} {ampm}
+                                {this.state.time.toLocaleTimeString()}
                         </h3>
                     </div>
                 );
