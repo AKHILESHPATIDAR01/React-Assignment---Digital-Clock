fc17b395ca0ef2d0ad3cf11ede5bc4f518eb8d4e
AKHILESHPATIDAR01 <71465599+AKHILESHPATIDAR01@users.noreply.github.com>
2021-03-01 16:21:23 +0530

    Update App.js
    cc

--- src/components/App.js
+++ src/components/App.js
@@ -2,47 +2,93 @@ import React, {Component, useState} from "react";
 import '../styles/App.css';
 // import Clock from "./Clock";
 
-function App(){
-    let time = new Date().toLocaleTimeString();
+// function App(){
+//     let time = new Date().toLocaleTimeString();
         
-    const [displayTime , setDisplayTime] = useState(time);
+//     const [displayTime , setDisplayTime] = useState(time);
     
-    // componentDidMount(){
-    //     setInterval(updateTime,1000);
-    // };
+//     // componentDidMount(){
+//     //     setInterval(updateTime,1000);
+//     // };
 
-    var timeer = setInterval(updateTime,1000);
+//     var timeer = setInterval(updateTime,1000);
 
-        // setInterval(()=>{
-        //     time = new Date().toLocaleTimeString();
-        //     // console.log(time);
-        //     setDisplayTime(time);
-        // },1000);
+//         // setInterval(()=>{
+//         //     time = new Date().toLocaleTimeString();
+//         //     // console.log(time);
+//         //     setDisplayTime(time);
+//         // },1000);
 
-        // let time = new Date();
+//         // let time = new Date();
 
         
 
-        function updateTime(){
-            time = new Date().toLocaleTimeString();
-            // console.log(time);
-            setDisplayTime(time);
+//         function updateTime(){
+//             time = new Date().toLocaleTimeString();
+//             // console.log(time);
+//             setDisplayTime(time);
+//         };
+
+
+//         // componentWillUnmount() {
+//         //     clearInterval(this.timeer);
+//         // }
+
+        
+
+//         return(
+//             <div className="Clock">
+//                <h3 id="time">{displayTime}</h3>
+//             </div>
+//         )
+    
+//     }
+
+
+class App extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = { 
+            date: new Date() 
         };
-
-
-        // componentWillUnmount() {
-        //     clearInterval(this.timeer);
-        // }
-
-        
-
-        return(
-            <div className="Clock">
-               <h3 id="time">{displayTime}</h3>
-            </div>
-        )
-    
     }
+
+    componentDidMount() {
+        this.interval = setInterval(
+            () => this.timer(),
+            1000
+        );
+    }
+
+    componentWillUnmount() {
+        clearInterval(this.interval);
+    }
+
+    timer() {
+        this.setState({
+            date: new Date()
+        });
+    }
+
+    render() {
+        let hours = this.state.date.getHours();
+        hours = hours % 12;
+        let minutes = this.state.date.getMinutes();
+        let seconds = this.state.date.getSeconds();
+        let ampm = this.state.date.getHours() >= 12 ? 'PM' : 'AM';
+        let result = ` ${hours}:${minutes}:${seconds}`
+        return (
+            <div className="Clock">
+                <h3 id="time">
+                    {result}{ampm}
+
+                </h3>
+            </div>
+        );
+    }
+}
+
+
 
 
 export default App;
